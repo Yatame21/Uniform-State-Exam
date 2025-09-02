8480843a40d04b52e085ea3ac79c009dbc645810
yatame21 <maximtoshov21@gmail.com>
2025-09-02 13:43:02 +0300

    two description

--- num_2/two.py
+++ num_2/two.py
@@ -1,17 +1,17 @@
-# ((y → x) ≡ (x → w)) ∧ (z ∨ x) 
+# здесь условие в коментах
 from itertools import product,permutations
 
-def f(x, y, z, w): # tyt
-    return ((y <= x) == (x <= w)) and (z or x) # tyt
+def f(x, y, z, w): # здесь пишешь все буквы которые есть в условии
+    return ((y <= x) == (x <= w)) and (z or x) # тут пишешь условие
 
-for x1,x2,x3,x4,x5,x6 in product([0,1], repeat=6): # tyt
+for x1,x2,x3,x4,x5,x6 in product([0,1], repeat=6): # тут пишешь сколько всего иксов (repeat=?)
 
-    t = ( # tyt
+    t = ( # в табице пишешь на месте пропусков иксы по типу x1,x2.... (F тоже пишешь)
         (0,x1,x2,0,1),
         (0,0,0,x3,1),
         (x4,x5,0,x6,1)
     )
     if len(t) == len(set(t)):
-        for p in permutations('xyzw', r=4): # tyt
+        for p in permutations('xyzw', r=4): # тут писать сколько всего букв (r=?)
             if all(f(**dict(zip(p, line))) == line[-1] for line in t):
                 print(*p)
