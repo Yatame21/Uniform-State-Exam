bcb461005f5d34a05e20e1e0e4454400dc1cad23
yatame21 <maximtoshov21@gmail.com>
2025-09-24 09:48:40 +0300

    full hm

---
+++ homework(210925)/3.4.py
@@ -0,0 +1,9 @@
+x = int(input())
+
+x1 = x % 10
+x2 = x // 10
+
+if x1 == x2:
+    print("да")
+else:
+    print("нет")
---
+++ homework(210925)/3.6.py
@@ -0,0 +1,8 @@
+x = int(input())
+
+if x < 100:
+    print("yes")
+elif x > -100:
+    print("yes")
+else:
+    print("no")
