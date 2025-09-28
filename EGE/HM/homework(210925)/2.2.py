207f6e48374300a9f36f46cc167b6d0b5847cf44
yatame21 <163657078+Yatame21@users.noreply.github.com>
2025-09-28 09:29:10 +0300

    check

---
+++ EGE/HM/homework(210925)/2.2.py
@@ -0,0 +1,6 @@
+x=float(input())
+y=float(input())
+
+f = ((x**3) - (2.5 * x * y) + (1.78 * (x**2)) - (2.5 * y) + 1)
+
+print(f)
---
+++ EGE/HM/homework(210925)/2.4.py
@@ -0,0 +1,10 @@
+a=int(input())
+b=int(input())
+
+perimeter = 2 * (a+b)
+square = a * b
+diagonal = ((a**2) + (b**2) )** 0.5
+
+print("Периметр:" , perimeter)
+print("Площадь:" , square)
+print("Диагональ:", diagonal)
