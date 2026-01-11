5f0762a954e0baeb33f9897cdc5cbbe04c0e4d1e
yatame21 <maximtoshov21@gmail.com>
2026-01-11 03:08:25 +0300

    ok, i dont care

--- main/main.py
+++ main/main.py
@@ -1,3 +1,13 @@
+
+
+
+def f(a,b,m):
+    if a+b >= 77: return m % 2 == 0
+    if m == 0:return 0
+    h = [f(a+1,b,m-1), f(a,b+1,m-1), f(a*2,b,m-1), f(a,b*2,m-1)]
+    return any(h) if m%2 != 0 else any(h)
+
+print([s for s in range(1,70)])
 def f(s,m):
     if s <= 16: return m%2 == 0
     if m == 0:return 0
